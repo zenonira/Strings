4298f4f61cc26f503204fdd263b6a7a6e03223d2
apcs <apcs@mvhs-u528ap03.dcsdk12.org>
2013-08-20 08:18:04 -0600

    degreesRadians

---
+++ degreesRadians.java
@@ -0,0 +1,11 @@
+class degreesRadians {
+	public static void main(String[] args) {
+		rad = 30.0 * Math.PI/180.0;
+		double sine = Math.sin(rad);
+		double cosine = Math.cos(rad);
+		double sineSquared = sine * sine;
+		double cosineSquared = cosine * cosine;
+		double answer = sineSquared + cosineSquared;
+		System.out.println(answer);
+	}
+}
