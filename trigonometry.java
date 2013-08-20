3fbb6a16a079495cb48de4502fa9c92a4e975d8e
apcs <apcs@mvhs-u528ap03.dcsdk12.org>
2013-08-20 08:08:59 -0600

    trigonometry

---
+++ trigonometry.java
@@ -0,0 +1,10 @@
+class trigonometry {
+	public static void main(String[] args) {
+		double sine = Math.sin(.5236);
+		double cosine = Math.cos(.5236);
+		double sineSquared = sine * sine;
+		double cosineSquared = cosine * cosine;
+		double answer = sineSquared + cosineSquared;
+		System.out.println(answer);
+	}
+}
