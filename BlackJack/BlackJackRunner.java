0b563709605c23aa16230556c3a7d3d7c1791729
zenonira <zenonira@s.dcsdk12.org>
2013-10-31 09:40:20 -0600

    Working on shuffle

---
+++ BlackJack/BlackJackRunner.java
@@ -0,0 +1,9 @@
+public class BlackJackRunner {
+	public static void main(String[] args) {
+		Deck newDeck = new Deck();
+		// while (Deck.timesDrawn == 52) {
+		// 	shuffle();
+		// }
+		shuffle();
+	}
+}
--- BlackJack/Deck.java
+++ BlackJack/Deck.java
@@ -1,17 +1,45 @@
+import java.util.Random;
 public class Deck {
 
 	Card[] cards = new Card[52];
+	int timesDrawn = 0;
 
 	public Deck() {
 		for (int x=1; x<=13; x++) {
-			Card newCard = new Card("heart", x);
-			cards[x-1] = newCard;
-			Card diamond = new Card("Diamond", x);
+			int i = x;
+			if (x>10) {
+				i = 10;
+			}
+			Card hearts = new Card("Heart", i);
+			cards[x-1] = hearts;
+			
+			Card diamond = new Card("Diamond", i);
 			cards[x+12] = diamond;
-			Card spades = new Card("Spades", x);
+			
+			Card spades = new Card("Spades", i);
 			cards[x+25] = spades;
-			Card clubs = new Card("Clubs", x);
+			
+			Card clubs = new Card("Clubs", i);
 			cards[x+38] = clubs;
+			
+		}
+ 	
+		shuffle();
+
+	}
+
+	public void drawCard() {
+		timesDrawn++;
+	}
+
+	public void shuffle() {
+		for (int i=52; i>=1; i--) {
+			Random random = new Random(52);
+			int b = random.nextInt(i);
+			Card a = cards[i];
+			cards[a] = cards[b];
+			cards[b] = a;
+
 		}
 	}
 
