c606c2d8d9078d062fc17b14a36fc905f1a4dfa7
zenonira <zenonira@s.dcsdk12.org>
2013-11-22 09:00:18 -0700

    11/21

--- BlackJack/TableApplet.java
+++ BlackJack/TableApplet.java
@@ -8,12 +8,27 @@ public class TableApplet extends Applet implements ActionListener{
 	Deck myDeck = new Deck();
 	Human human = new Human();
 	Dealer dealer = new Dealer();
+	private JLabel label;
 	private JButton hit, stay, newGame;
 	private String answer;
+	private int humanWallet = 1000;
+	// private int dealerWallet = 99999999;
+	private int pot = 0;
 
 
 	public void init() {
-		String title = "Hit";
+		label = new JLabel("You have $" + humanWallet + " the pot is $" + pot);
+		label.setFont(new Font("Sansserif", Font.BOLD, 32));
+		this.add(label);
+
+		String title = "Bet $10";
+		bet = new JButton(title);
+		bet.setActionCommand(title);
+		bet.addActionListener(this);
+		this.add(bet);
+
+
+		title = "Hit";
 		hit = new JButton(title);
 		hit.setActionCommand(title);
 		hit.addActionListener(this);
@@ -25,18 +40,13 @@ public class TableApplet extends Applet implements ActionListener{
 		stay.addActionListener(this);
 		this.add(stay);
 
-		title = "New Game";
+		title = "Next Hand";
 		newGame = new JButton(title);
 		newGame.setActionCommand(title);
 		newGame.addActionListener(this);
 		this.add(newGame);
-
-
-		myDeck.shuffle();
-		human.add(myDeck.top());
-		dealer.add(myDeck.top());
-		human.add(myDeck.top());
-		dealer.add(myDeck.top());
+		
+		game();
 
 
 	
@@ -44,6 +54,10 @@ public class TableApplet extends Applet implements ActionListener{
 	}
 
 	public void actionPerformed(ActionEvent ae) {
+		if ("Bet $10".equals(ae.getActionCommand())) {
+			humanWallet -= 10;
+			pot += 20;
+		}
 		if ("Hit".equals(ae.getActionCommand())) {
 			if (human.getTotal() <= 21) {
 				human.add(myDeck.top());
@@ -54,23 +68,42 @@ public class TableApplet extends Applet implements ActionListener{
 			}
 			repaint();
 		} else if ("Stay".equals(ae.getActionCommand())) {
-			if (dealer.getTotal() < 17) {
-				dealer.add(myDeck.top());
-			} else {
-				if ((human.getTotal() > dealer.getTotal() || dealer.getTotal() > 21) && human.getTotal() < 22) {
-					answer = "You Win";
-				} else if (human.getTotal() < dealer.getTotal() && dealer.getTotal() < 22 || human.getTotal() > 21) {
-					answer = "You Lose";
-				} else {
-					answer = "It's A Draw";
+			for (int i=0; i<11; i++) {
+				if (dealer.getTotal() <= 17) {
+					dealer.add(myDeck.top());	
 				}
+			}
+			if ((human.getTotal() > dealer.getTotal() || dealer.getTotal() > 21) && human.getTotal() < 22) {
+				answer = "You Win";
+				humanWallet += pot;
+			} else if (human.getTotal() < dealer.getTotal() && dealer.getTotal() < 22 || human.getTotal() > 21) {
+				answer = "You Lose";
+			} else {
+				answer = "It's A Draw";
+				humanWallet += pot/2;
 			}
 			repaint();
 		}
-		if ("New Game".equals(ae.getActionCommand())) {
-			init();
+		if ("Next Hand".equals(ae.getActionCommand())) {
+			answer = "";
+			game();
+			pot = 0;
 			repaint();
 		}
+	}
+
+	public void game() {
+		Deck newDeck = new Deck();
+		Human newHuman = new Human();
+		Dealer newDealer = new Dealer();
+		myDeck = newDeck;
+		human = newHuman;
+		dealer = newDealer;
+		myDeck.shuffle();
+		human.add(myDeck.top());
+		dealer.add(myDeck.top());
+		human.add(myDeck.top());
+		dealer.add(myDeck.top());
 	}
 
 	public void checkAnswer() {
@@ -87,7 +120,7 @@ public class TableApplet extends Applet implements ActionListener{
 		dealer.draw(g);
 		g.drawString("" + human.getTotal(), 25, 25);
 		g.drawString("" + dealer.getTotal(), 25, 50);
-		g.drawString(answer, 100, 400);
+		g.drawString("" + answer, 100, 400);
 	}
 
 }
